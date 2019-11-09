4785a9b3289fc99205204a1c402b25e2b1238c16
mehrabsha <mehrab572@gmail.com>
2019-11-09 13:33:48 +0330

    cross browser

--- Demo/js/main.js
+++ Demo/js/main.js
@@ -1,59 +1,109 @@
+"use strict";
+
 var ppposition = 'bottom-left';
-         
 $(document).ready(function () {
-
-
-    init({
-        fade_in : 800 ,
-        fade_out : 800 ,
-        position : ppposition
+  init({
+    fade_in: 800,
+    fade_out: 800,
+    position: ppposition
+  });
+  document.getElementById("btnSm").addEventListener("click", function () {
+    toast({
+      title: 'Hi',
+      description: 'Success message'
     });
-    
-    document.getElementById("btnSm").addEventListener("click",()=>{toast({title:'Hi' , description: 'Success message' })});
-    document.getElementById("btnEm").addEventListener("click",()=>{toast({title:'Hi' , description: 'Error message'  , type: 'error'})});
-    document.getElementById("btnWm").addEventListener("click",()=>{toast({title:'Hi' , description: 'Warning message'  , type: 'warning'})});
-    document.getElementById("btnMt").addEventListener("click",()=>{toast({title:'Hi' , description: 'Message with timeout' , type: 'success' , timeout : 1000})});
-    document.getElementById("btnCt").addEventListener("click",()=>{toast({title:'Hi' , description: 'Custom message' , timeout : 0 ,type : 'custom' ,close_color: '#3498db' ,border_color: '#3498db' , icon : 'url(https://img.icons8.com/flat_round/2x/info.png)'})});
-    document.getElementById("btnCtNi").addEventListener("click",()=>{toast({title:'Hi' , description: 'Custom message (whitout icon)' , timeout : 0 ,type : 'custom' ,close_color: '#3498db' ,border_color: '#3498db'})});
-    document.getElementById("btnHt").addEventListener("click",()=>{toast({title:'<span style="color:red">Hi</span>' , description: '<b>HTML</b> toast message' })});
-    document.getElementById("btnDs").addEventListener("click",()=>{toast({title:'<span style="color:#f9f9f9">Hi</span>' , description: '<span style="color:#bdc3c7">Dark style toast message</span>' ,close_color: '#f1c40f' ,border_color: '#f1c40f' ,color : '#212121' ,type:'custom' })});
-   
+  });
+  document.getElementById("btnEm").addEventListener("click", function () {
+    toast({
+      title: 'Hi',
+      description: 'Error message',
+      type: 'error'
+    });
+  });
+  document.getElementById("btnWm").addEventListener("click", function () {
+    toast({
+      title: 'Hi',
+      description: 'Warning message',
+      type: 'warning'
+    });
+  });
+  document.getElementById("btnMt").addEventListener("click", function () {
+    toast({
+      title: 'Hi',
+      description: 'Message with timeout',
+      type: 'success',
+      timeout: 1000
+    });
+  });
+  document.getElementById("btnCt").addEventListener("click", function () {
+    toast({
+      title: 'Hi',
+      description: 'Custom message',
+      timeout: 0,
+      type: 'custom',
+      close_color: '#3498db',
+      border_color: '#3498db',
+      icon: 'url(https://img.icons8.com/flat_round/2x/info.png)'
+    });
+  });
+  document.getElementById("btnCtNi").addEventListener("click", function () {
+    toast({
+      title: 'Hi',
+      description: 'Custom message (whitout icon)',
+      timeout: 0,
+      type: 'custom',
+      close_color: '#3498db',
+      border_color: '#3498db'
+    });
+  });
+  document.getElementById("btnHt").addEventListener("click", function () {
+    toast({
+      title: '<span style="color:red">Hi</span>',
+      description: '<b>HTML</b> toast message'
+    });
+  });
+  document.getElementById("btnDs").addEventListener("click", function () {
+    toast({
+      title: '<span style="color:#f9f9f9">Hi</span>',
+      description: '<span style="color:#bdc3c7">Dark style toast message</span>',
+      close_color: '#f1c40f',
+      border_color: '#f1c40f',
+      color: '#212121',
+      type: 'custom'
+    });
+  });
 
-    function pposition(){
-        ppposition = 'top-center';
-        init({
-            position: ppposition
-        });
-        toast({title:'Hi' , description: 'Success message' });
-    }
-    
-    document.getElementById("btnCp").addEventListener("click",()=>{pposition()} );
+  function pposition() {
+    ppposition = 'top-center';
+    init({
+      position: ppposition
+    });
+    toast({
+      title: 'Hi',
+      description: 'Success message'
+    });
+  }
 
-
-
-
-$(".tag").click(function (e) { 
+  document.getElementById("btnCp").addEventListener("click", function () {
+    pposition();
+  });
+  $(".tag").click(function (e) {
     console.log();
-    
     $('html').animate({
-        scrollTop: $('#' +$(this).attr('target')).offset().top
+      scrollTop: $('#' + $(this).attr('target')).offset().top
     }, 1000);
-});
-
-
-
-
-
-
-$(document).scroll(function () { 
-    if ($(document).scrollTop() > 400){
-     $("#toTop")[0].style.opacity='1.0';
-    }else{  
-     $("#toTop")[0].style.opacity='0.0';
+  });
+  $(document).scroll(function () {
+    if ($(document).scrollTop() > 400) {
+      $("#toTop")[0].style.opacity = '1.0';
+    } else {
+      $("#toTop")[0].style.opacity = '0.0';
     }
- });
+  });
 });
 
 function toTop() {
-    $('html,body').animate({ scrollTop: 0 }, 'slow');
+  $('html,body').animate({
+    scrollTop: 0
+  }, 'slow');
 }
--- src/toast.js
+++ src/toast.js
@@ -1,152 +1,147 @@
+"use strict";
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
 if (true) {
-  
-let fade_in = 800;
-let fade_out = 800;
-let position = 'bottom-left';
-let h;
-let i = 0;
+  var init = function init(object) {
+    var _ = object;
 
-
-if (document.getElementsByClassName("msha_toast_container").length == 0) {
-    document.body.insertAdjacentHTML("beforeend", '<div class="msha_toast_container"></div>');
-}
-
-function init(object) {
-    let _ = object;
-    if (typeof object == 'object') {
-        fade_out = _.fade_out != null ? _.fade_out : fade_out;
-        fade_in = _.fade_in != null ? _.fade_in : fade_in;
-        position = _.position != null ? _.position : position;
+    if (_typeof(object) == 'object') {
+      fade_out = _.fade_out != null ? _.fade_out : fade_out;
+      fade_in = _.fade_in != null ? _.fade_in : fade_in;
+      position = _.position != null ? _.position : position;
     }
-    let e = document.getElementsByClassName("msha_toast_container")[0];
+
+    var e = document.getElementsByClassName("msha_toast_container")[0];
+
     switch (position) {
-        case 'top-left':
-            e.className = 'msha_toast_container';
-            e.setAttribute("style", "");
-            e.classList.add(position);
-            break;
+      case 'top-left':
+        e.className = 'msha_toast_container';
+        e.setAttribute("style", "");
+        e.classList.add(position);
+        break;
 
-        case 'top-center':
-            e.className = 'msha_toast_container';
-            e.style.top = 0;
-            e.style.left = '50%';
-            e.style.transform = 'translateX(-50%)';
-            break;
+      case 'top-center':
+        e.className = 'msha_toast_container';
+        e.style.top = 0;
+        e.style.left = '50%';
+        e.style.transform = 'translateX(-50%)';
+        break;
 
-        case 'top-right':
-            e.className = 'msha_toast_container';
-            e.setAttribute("style", "");
-            e.classList.add(position);
-            break;
+      case 'top-right':
+        e.className = 'msha_toast_container';
+        e.setAttribute("style", "");
+        e.classList.add(position);
+        break;
 
-        case 'bottom-left':
-            e.className = 'msha_toast_container';
-            e.setAttribute("style", "");
-            e.classList.add(position);
+      case 'bottom-left':
+        e.className = 'msha_toast_container';
+        e.setAttribute("style", "");
+        e.classList.add(position);
+        break;
 
-            break;
+      case 'bottom-center':
+        e.className = 'msha_toast_container';
+        e.style.bottom = 0;
+        e.style.left = '50%';
+        e.style.transform = 'translateX(-50%)';
+        break;
 
-        case 'bottom-center':
-            e.className = 'msha_toast_container';
-            e.style.bottom = 0;
-            e.style.left = '50%';
-            e.style.transform = 'translateX(-50%)';
-            break;
+      case 'bottom-right':
+        e.className = 'msha_toast_container';
+        e.setAttribute("style", "");
+        e.classList.add(position);
+        break;
 
-        case 'bottom-right':
-            e.className = 'msha_toast_container';
-            e.setAttribute("style", "");
-            e.classList.add(position);
-
-            break;
-
-        default:
-            e.style.bottom = 0;
-            e.style.left = 0;
-            e.style.transform = null;
-            break;
+      default:
+        e.style.bottom = 0;
+        e.style.left = 0;
+        e.style.transform = null;
+        break;
     }
-}
+  };
 
-function toast(object) {
-    let toast_timeout = 0;
-    let toast_desc = "Toast message";
-    let toast_title = "Hi";
-    let toast_type = "success";
-    let toast_color = "#fff"
-    let toast_close_color = "#999";
+  var toast = function toast(object) {
+    var toast_timeout = 0;
+    var toast_desc = "Toast message";
+    var toast_title = "Hi";
+    var toast_type = "success";
+    var toast_color = "#fff";
+    var toast_close_color = "#999";
+    var _ = object; //Custom
 
+    var bordercolor = "#43d569";
+    var icon;
+    var radius = 3;
 
-    let _ = object;
-
-    //Custom
-    let borderColor = "#43d569";
-    let icon;
-    let radius = 3;
-
-
-    if (typeof object == 'object') {
-        toast_desc = _.description != null && _.description.length > 0 ? _.description : toast_desc;
-        toast_title = _.title != null && _.title.length > 0 ? _.title : toast_title;
-        toast_timeout = _.timeout != null ? _.timeout : toast_timeout;
-        toast_type = _.type != null ? _.type : toast_type;
-        toast_color = _.color!=null ? _.color : toast_color;
-        toast_close_color = _.close_color!=null ? _.close_color : toast_close_color;
+    if (_typeof(object) == 'object') {
+      toast_desc = _.description != null && _.description.length > 0 ? _.description : toast_desc;
+      toast_title = _.title != null && _.title.length > 0 ? _.title : toast_title;
+      toast_timeout = _.timeout != null ? _.timeout : toast_timeout;
+      toast_type = _.type != null ? _.type : toast_type;
+      toast_color = _.color != null ? _.color : toast_color;
+      toast_close_color = _.close_color != null ? _.close_color : toast_close_color;
     }
 
-    document.getElementsByClassName("msha_toast_container")[0].insertAdjacentHTML("beforeend", `<div class="msha_toast ${toast_type}"><div class="msha_toast_icon msha_${toast_type}_icon">    </div><div class="msha_toast_contents"><div class="msha_toast_title">${toast_title}</div><div class="msha_toast_desc"> ${toast_desc} </div></div><div class="msha_toast_close" onclick="del()">✖</div>`);
-    let toast = document.getElementsByClassName("msha_toast")[i];
-    toast.style.animation = `fadeIn ${(fade_in/1000)}s`;
+    document.getElementsByClassName("msha_toast_container")[0].insertAdjacentHTML("beforeend", "<div class=\"msha_toast ".concat(toast_type, "\"><div class=\"msha_toast_icon msha_").concat(toast_type, "_icon\">    </div><div class=\"msha_toast_contents\"><div class=\"msha_toast_title\">").concat(toast_title, "</div><div class=\"msha_toast_desc\"> ").concat(toast_desc, " </div></div><div class=\"msha_toast_close\" onclick=\"del()\">\u2716</div>"));
+    var toast = document.getElementsByClassName("msha_toast")[i];
+    toast.style.animation = "fadeIn ".concat(fade_in / 1000, "s");
+    toast.style.WebkitAnimation = "fadeIn ".concat(fade_in / 1000, "s");
+    toast.style.MozAnimation = "fadeIn ".concat(fade_in / 1000, "s");
     toast.style.backgroundColor = toast_color;
     toast.getElementsByClassName("msha_toast_close")[0].style.color = toast_close_color;
 
-
     if (toast_type == "custom") {
-        bordercolor = _.border_color!=null ? _.border_color : borderColor;
-        icon = _.icon!=null ? _.icon : icon;
-        radius = _.radius!=null ? _.radius : radius;
-        console.log(bordercolor);
-        
-        toast.style.borderLeft = `5px solid ${bordercolor}`;
-        toast.style.borderRadius = `${radius}px`;
-        icon!=null ? toast.getElementsByClassName("msha_custom_icon")[0].style.background = icon : toast.getElementsByClassName("msha_custom_icon")[0].style.display = 'none' ;
-
+     bordercolor = _.border_color != null ? _.border_color : bordercolor;
+      icon = _.icon != null ? _.icon : icon;
+      radius = _.radius != null ? _.radius : radius;
+      console.log(bordercolor);
+      toast.style.borderLeft = "5px solid ".concat(bordercolor);
+      toast.style.borderRadius = "".concat(radius, "px");
+      icon != null ? toast.getElementsByClassName("msha_custom_icon")[0].style.background = icon : toast.getElementsByClassName("msha_custom_icon")[0].style.display = 'none';
     }
-    
-
-
 
     if (toast_timeout != null && toast_timeout > 0) {
-        var c = document.getElementsByClassName("msha_toast")[i];
-        setTimeout(() => {
-            del(null, c)
-        }, (toast_timeout + fade_in));
-    }
+      var c = document.getElementsByClassName("msha_toast")[i];
+      setTimeout(function () {
+        del(null, c);
+      }, toast_timeout + fade_in);
+    } //display block
 
-    //display block
+
     i++;
-}
+  };
 
-function del(e, ee = null) {
-    let x;
+  var del = function del(e) {
+    var ee = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
+    var x;
+
     if (ee != null) {
-        x = ee;
+      x = ee;
     } else {
-        x = event.target.parentElement;
+      x = event.target.parentElement;
     }
 
-
-    x.style.WebkitAnimation = `fadeOut ${(fade_out/1000)}s forwards`;
+    x.style.WebkitAnimation = "fadeOut ".concat(fade_out / 1000, "s forwards");
+    x.style.MozAnimation = "fadeOut ".concat(fade_out / 1000, "s forwards");
+    x.style.animation = "fadeOut ".concat(fade_out / 1000, "s forwards");
     console.log(x.classList);
-
-    setTimeout(() => {
-        x.classList.add("remove");
+    setTimeout(function () {
+      x.classList.add("remove");
     }, fade_out);
-    setTimeout(() => {
-        x.remove();
-        i-- ? i != 0: null;
+    setTimeout(function () {
+      x.remove();
+      i-- ? i != 0 : null;
     }, fade_out + 300);
+  };
 
+  var fade_in = 800;
+  var fade_out = 800;
+  var position = 'bottom-left';
+  var h;
+  var i = 0;
 
-}  
+  if (document.getElementsByClassName("msha_toast_container").length == 0) {
+    document.body.insertAdjacentHTML("beforeend", '<div class="msha_toast_container"></div>');
+  }
 }
