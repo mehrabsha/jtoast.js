971f927767af7728459e3e618a88f1b32116723c
mehrabsha <44315888+mehrabsha@users.noreply.github.com>
2019-11-10 12:22:56 +0330

    Update toast.js

--- src/toast.js
+++ src/toast.js
@@ -3,7 +3,7 @@
 function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 
-  var init = function init(object) {
+  var $init = function $init(object) {
     var _ = object;
 
     if (_typeof(object) == 'object') {
